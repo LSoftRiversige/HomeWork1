bae8b2ca2c8bcbbb017fba4ee4a3c4bbc029a996
lsoft.riverside <lsoft.riverside@gmail.com>
2018-11-14 10:56:53 +0200

    Add project files.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Describe class with 2-5 fields/properties, implement at least 2 methods, 1 of them had to be overriten from the base class. 
+//fields and methods had to be logical according to data domain(ру: предеметная область).
+
+//List of topics. (Tasks in topics can be classes or objects)
+//1. Wall, Roof, House, Eiffel Tower, Big Ben
+//2. File, Directory, Image, Document, word.doc, img.png
+//3. Book, Author, Newspaper, Song, ArtWork, J.R.R.Tolkien, Lord of the rings
+//4. decimal, Type, Class, Struct, Enum, Variable, Method, int
+//5. Pulp fiction, H8tfull eight, Director, Film, Book, Artwork, Actor, Role
+//6. с#, programming language, assembler, programmer
+//7. "The Old Man and the Sea" , Message, Person, Note, Bill, Book, Poem, Ernest Miller Hemingway
+//8. Project, Program.cs, Solution, Source file, CSharpConsoleApplication1, CSharp Project, Visual Basic Project
+//9. Person, Sport, Olimpic games, Medal, Sportsman, Boxing, Basketball, Michael Gerard «Mike» Tyson, Olimpic games in Rio
+//10. Human, Leg, Hand, Body, Head, Morgan Freeman, Bill Gates, Negro, White.
+//11. Univercity, Student, Professor Donald Ervin Knuth, Pupil, School, Professor, Person, College, Institution(ру: Учебное заведение), Oxford
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
